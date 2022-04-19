e69fbca811941ed729b21100b1c3cf37ac477699
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-04-19 00:33:24 -0500

    manager prompt block now logs and pushes to employee array then triggers questions about engineers. Engineers prompt block now logs and pushes information into employee array. Changed pseudocoding doc to .txt so it was easier to read. Moved node modules to main level to be able to access them to call inquirer.

--- index.js
+++ index.js
@@ -1,36 +1,139 @@
-const Employee = require('./javascript/employee');
-const Manager = require('./javascript/manager');
-const Engineer = require('./javascript/engineer');
-const Intern = require('./javascript/intern');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const inquirer = require('inquirer')
 const employeeArr = [];
 
-const userInput = () => {
+const managerInput = () => {
   return inquirer.prompt([
     {
-      name:"team",
-      type:"confirm",
-      message:"Would you like to create a team?",
+      name:"name",
+      type:"input",
+      message:"What is your manager's name?",
     },
     {
-      name:"employeeType",
-      type:"list",
-      message:"Which of these employee types would you like to add?",
-      choices:["engineer","intern"]
-    }
-  ])}
+      name:"id",
+      type:"input",
+      message:"Please enter your manager's employee ID number.",
+    },
+    {
+      name:"email",
+      type:"input",
+      message:"Please enter your manager's e-mail address."
+    },
+    {
+      name:"officeNumber",
+      type:"input",
+      message:"Please enter your manager's office number."
+    },
 
-//a function to generate markdown for HTML
-function generateHTML(answers) {
-  // console.log(answers)
-  return  
-};
+  ]).then(answers=>{
+    console.log(answers);
+    const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
+    console.log(manager);
+    employeeArr.push(manager);
+    console.log(employeeArr);
+    engineerInput();
+  });
+}
 
-module.exports = generateHTML;
+  const engineerInput = () => {
+    return inquirer.prompt([
+  {
+    name:"name",
+    type:"input",
+    message:"Please enter your engineer's name.", 
+  },
+  {
+    name:"id",
+    type:"input",
+    message:"Please enter your engineer's ID number.",  
+  },
+  {
+    name:"email",
+    type:"input",
+    message:"Please enter your engineer's e-mail address.", 
+  },
+  {
+    name:"gitHub",
+    type:"input",
+    message:"Please enter your engineer's GitHub username.", 
+  },
+    ]) .then(answers=>{
+      console.log(answers);
+      const engineer = new Engineer (answers.name, answers.id, answers.email, answers.gitHub);
+      console.log(engineer);
+      employeeArr.push(engineer);
+      console.log(employeeArr);
+      internInput();
+  });
+  }
+//     const internInput = () => {
+//       return inquirer.prompt([
+//   {
+//     name:"internName",
+//     type:"input",
+//     message:"Please enter your intern's name.", 
+//   },
+//   {
+//     name:"internID",
+//     type:"input",
+//     message:"Please enter your intern's ID number.",  
+//   },
+//   {
+//     name:"internEmail",
+//     type:"input",
+//     message:"Please enter your intern's e-mail address.",  
+//   },
+//   {
+//     name:"internSchool",
+//     type:"input",
+//     message:"Please enter the name of your intern's school.",  
+//   },
+// ]) moreEmployeeInput ();
+// };
+
+// const moreEmployeeInput = () => {
+//   return inquirer.prompt([
+// {
+//   name:"more",
+//   type:"confirm",
+//   message:"Would you like to add more employees?",
+// },
+// ]) if{
+    // if (answers.more == true) {
+    //   employeeTypeInput();
+    // } else {
+    //   generateHTML();
+    // }
+// }
+// };
+
+// const employeeTypeInput = () => {
+//   return inquirer.prompt([
+// {
+//   name:"employeeType",
+//   type:"list",
+//   message:"Which of these employee types would you like to add?",
+//   choices:["engineer","intern"],
+// },
+//   ])};
+
+// module.exports = generateHTML;
 
 const init = () => {
-  userInput()
-  .then((answers) => fs.writeFileSync('./dist/renderedoutput.HTML', generateHTML(answers)))
-  .then(() => console.log('Successfully wrote to HTML'))
-  .catch((err) => console.error(err));
+  managerInput()
+  // .then((answers) => fs.writeFileSync('./dist/renderedoutput.HTML', generateHTML(answers)))
+  .then((answers)=> console.log('you did it, buddy'))
+  // .then(() => console.log('Successfully wrote to HTML'))
+  .catch((err) => console.error(err))
 };
 init();
+
+// // //a function to generate HTML
+// const generateHTML = () => {
+//   console.log(employeeArr)
+// //   return  
+// };
+// generateHTML();
