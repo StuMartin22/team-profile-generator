847b0e493c2853646d17f467ae85f310450e5bd4
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-04-18 19:00:58 -0500

    added classes for employee, manager, engineer and intern. Also created exports for all the files as well as extended the employee class to all the different employee types.

---
+++ engineer.js
@@ -0,0 +1,15 @@
+const Employee = require("./employee");
+class Engineer extends Employee {
+  constructor(name, id, email, gitHub ) {
+    super(name, id, email);
+    this.gitHub = gitHub;
+  }
+  getRole() {
+    return "Engineer";
+  }
+  getgitHub() {
+      return this.gitHub;
+  }
+};
+
+module.exports = Engineer
---
+++ index.js
@@ -0,0 +1,13 @@
+const Employee = require('./employee');
+const Manager = require('./manager');
+const Engineer = require('./engineer');
+const Intern = require('./intern');
+const employeeArr = [];
+
+//a function to generate markdown for README
+function generateHTML(answers) {
+  console.log(answers)
+  return  
+};
+
+module.exports = generateHTML;
