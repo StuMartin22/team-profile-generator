667a37410725a0981d2d8c6be737c076c570f435
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-04-18 23:10:08 -0500

    added filters for gitignore file. began working on logic for inquirer prompts. Added basic file structure for testing.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const Employee = require('./employee');
-const Manager = require('./manager');
-const Engineer = require('./engineer');
-const Intern = require('./intern');
+const Employee = require('./javascript/employee');
+const Manager = require('./javascript/manager');
+const Engineer = require('./javascript/engineer');
+const Intern = require('./javascript/intern');
 const employeeArr = [];
 
-//a function to generate markdown for README
+const userInput = () => {
+  return inquirer.prompt([
+    {
+      name:"team",
+      type:"confirm",
+      message:"Would you like to create a team?",
+    },
+    {
+      name:"employeeType",
+      type:"list",
+      message:"Which of these employee types would you like to add?",
+      choices:["engineer","intern"]
+    }
+  ])}
+
+//a function to generate markdown for HTML
 function generateHTML(answers) {
-  console.log(answers)
+  // console.log(answers)
   return  
 };
 
 module.exports = generateHTML;
+
+const init = () => {
+  userInput()
+  .then((answers) => fs.writeFileSync('./dist/renderedoutput.HTML', generateHTML(answers)))
+  .then(() => console.log('Successfully wrote to HTML'))
+  .catch((err) => console.error(err));
+};
+init();
